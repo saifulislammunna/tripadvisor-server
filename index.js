1a6255ad714c2bfd49df763ae6e95c26b876853e
saifulislammunna <saifulmunna28@gmail.com>
2021-10-29 00:51:43 +0600

    server-site-create

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const { MongoClient } = require('mongodb');
+const cors = require('cors');
+require('dotenv').config()
+
+const app = express()
+const port =  process.env.PORT || 5000
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u6dke.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+console.log(uri);
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run(){
+    try{
+        await client.connect();
+        console.log('database connected');
+        const database = client.db('tripAdvisor');
+        const serviceCollection = database.collection('services');
+
+        
+
+        // GET Products API 
+        app.get('/services', async(req, res) => {
+            const cursor = serviceCollection.find({});
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+
+    }
+    finally{
+        // await client.close()
+    }
+
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
